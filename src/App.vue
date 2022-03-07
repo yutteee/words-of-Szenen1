2254110d96825b1a1d96ccf66b13f0cacdc231c7
yutteee <yusaku_nakamura_aa@icloud.com>
2022-03-08 00:11:34 +0900

    header,fotter,sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,93 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    <v-navigation-drawer app clipped v-model="drawer" width>
+      <v-container>
+        <v-list-item>Iinhalt(もくじ)</v-list-item>
+        <v-divider></v-divider>
+        <v-list dense nav>
+          <v-list-item v-for="unit in units" :key="unit.number">
+            <v-list>{{ unit.title }}</v-list>
+          </v-list-item>
+        </v-list>
+      </v-container>
+    </v-navigation-drawer>
+    <v-app-bar color="#008000" app dark clipped-left>
+      <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Szenen1 Words</v-toolbar-title>
     </v-app-bar>
-
-    <v-main>
-      <router-view/>
-    </v-main>
+    <v-footer color="#a9a9a9" app>参考: Szenen1 heute akutuell 場面で学ぶドイツ語  三修社</v-footer>
   </v-app>
 </template>
 
 <script>
 
 export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
+  data(){
+    return{
+      drawer: null,
+      units: [
+        {
+          number: 1,
+          title: "1 Kennenlernen",
+          link: "#"
+        },
+        {
+          number: 2,
+          title: "2 Länder, Sprachen, Studium",
+          link: "#"
+        },
+        {
+          number: 3,
+          title: "3 Essen und Trinken",
+          link: "#"
+        },
+        {
+          number: 4,
+          title: "4 Freizeit und Hobbys",
+          link: "#"
+        },
+        {
+          number: 5,
+          title: "5 Familie und Berufe",
+          link: "#"
+        },
+        {
+          number: 6,
+          title: "6 Gegenstände und Einkaufen",
+          link: "#"
+        },
+        {
+          number: 7,
+          title: "7 Nach den Ferien",
+          link: "#"
+        },
+        {
+          number: 8,
+          title: "8 Wohnen",
+          link: "#"
+        },
+        {
+          number: 9,
+          title: "Uhrzeit und Datum",
+          link: "#"
+        },
+        {
+          number: 10,
+          title: "10 Stadt und Datum",
+          link: "#"
+        },
+        {
+          number: 11,
+          title: "11 Vor den Ferien",
+          link: "#"
+        },
+        {
+          number: 12,
+          title: "12 Erlebnisse und Erfahrungen",
+          link: "#"
+        }
+      ]
+    }
+  }
 };
 </script>
