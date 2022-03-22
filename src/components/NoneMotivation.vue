6ff116bbe574a0503270edb13ad0e14a259001ff
yutteee <yusaku_nakamura_aa@icloud.com>
2022-03-22 13:43:01 +0900

    未完成ページのコンポーネントのデザイン

---
+++ src/components/NoneMotivation.vue
@@ -0,0 +1,22 @@
+<template>
+<v-container class="center">
+    <v-img src="../assets/motivation.png" width="250px" class="image"></v-img>
+    <p>製作者のモチベが低いよ!応援してくれたら作ってくれる可能性が高いよ!</p>
+    <v-btn>応援</v-btn>
+</v-container>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.center {
+    text-align: center;
+}
+
+.image {
+    display: inline-block;
+}
+
+</style>
