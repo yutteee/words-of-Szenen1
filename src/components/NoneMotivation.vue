3770d9f2055d772a9072678c8b71403fdb949e62
yutteee <yusaku_nakamura_aa@icloud.com>
2022-03-22 15:09:55 +0900

    ボタンカウント追加

--- src/components/NoneMotivation.vue
+++ src/components/NoneMotivation.vue
@@ -2,12 +2,24 @@
 <v-container class="center">
     <v-img src="../assets/motivation.png" width="250px" class="image"></v-img>
     <p>製作者のモチベが低いよ!応援してくれたら作ってくれる可能性が高いよ!</p>
-    <v-btn>応援</v-btn>
+    <v-btn @click="incrementTotal">応援</v-btn>
+    <div>total:{{ total }}</div>
 </v-container>
 </template>
 
 <script>
-
+export default {
+    data(){
+        return {
+            total: 0
+        }
+    },
+    methods: {
+        incrementTotal: function(){
+            this.total++
+        }
+    }
+}
 </script>
 
 <style scoped>
